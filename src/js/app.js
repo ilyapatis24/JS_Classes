878b15989ad80e56dae8613c43d9ac96dfe2194f
ilyapatis24 <ilyapatis24@mail.ru>
2024-10-25 21:25:43 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+"use strict";
+import { Character, Bowerman, Swordsman, Magician, Daemon, Undead, Zombie } from './heroes.js';
+
+console.log(new Character('Crusider', 'Swordsman'));
+console.log(new Bowerman('Boris'));
+console.log(new Swordsman('Boris'));
+console.log(new Magician('Boris'));
+console.log(new Daemon('Boris'));
+console.log(new Undead('Boris'));
+console.log(new Zombie('Boris'));
